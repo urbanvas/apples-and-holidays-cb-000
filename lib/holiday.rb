a1aaea1aa339e2491cb34c89b01be8ef889e71c3
Neil Seejoor <nvseejoor@gmail.com>
2018-02-09 04:55:38 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,6 +49,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  holiday_hash[:winter] << ["Lights", "Wreath", "Party Hats"]
 
 end
 
